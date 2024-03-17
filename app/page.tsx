fe1557c3570d90dae8335f51e91e4447e5f509b4
tinnd <tinndev21@gmail.com>
2024-03-17 23:30:58 +0700

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import { Content } from "@/components/home/content";
-import Dashboard from "./dashboard/page";
 import Miner from "./miner/page";
 
 const Home: NextPage = () => {
